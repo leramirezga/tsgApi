19f4664593d775d458e1c75413b3fcb551d02592
leramirezga <74741831+leramirezga@users.noreply.github.com>
2025-05-12 15:00:38 -0500

    Cambio de ejemplo para PR: se agregó un comentario sencillo en PruebaGithubPackagesApplication.java

---
+++ src/main/java/prueba_github_packages/PruebaGithubPackagesApplication.java
@@ -0,0 +1,14 @@
+package prueba_github_packages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Este es un cambio de ejemplo para un PR
+@SpringBootApplication
+public class PruebaGithubPackagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaGithubPackagesApplication.class, args);
+	}
+
+}
